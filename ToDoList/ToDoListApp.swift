457e0718c4820720fb23b4e57a47b24ba238dcbe
IDONTCRYQAQ <omgisgg@gmail.com>
2023-05-12 10:49:52 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by csapp sicp on 2023/5/12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
